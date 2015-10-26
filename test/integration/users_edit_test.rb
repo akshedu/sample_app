2acc4ef84f43b095446f5121ec9250f72cd25171
akshedu <akshedu@gmail.com>
2015-10-27 01:24:34 +0530

    Finish user CRUD

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+ 
+ def setup
+       @user = users(:akshansh)
+   end
+
+	test "unsuccessful edit" do
+		log_in_as(@user)
+		get edit_user_path(@user)
+		assert_template 'users/edit'
+		patch user_path(@user), user: {name: '', email: 'hello@invalid', password: 'hi', password_confirmation: 'hello'}
+		assert_template 'users/edit'	
+	end
+
+	test "successful edit with friendly forwarding" do
+		get edit_user_path(@user)
+		log_in_as(@user)
+		assert_redirected_to edit_user_path(@user)
+		log_in_as(@user)
+		get edit_user_path(@user)
+		assert_template 'users/edit'
+		name = "Shalmali Patkar"
+		email = "shalmali12@sampleapp.com"
+		patch user_path(@user), user: {name: name, email: email, password: "", password_confirmation: ""}	
+		assert_not flash.empty?
+		follow_redirect!
+		assert_template 'users/show'
+		@user.reload
+		assert_equal @user.name, name
+		assert_equal @user.email, email
+	end
+
+end
