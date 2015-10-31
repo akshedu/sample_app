6d38d6e7193691f7347e0d9dec5d82314b39d880
akshedu <akshedu@gmail.com>
2015-11-01 00:44:32 +0530

    changed carrier wave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,8 +1,9 @@
 if Rails.env.production?
 	CarrierWave.configure do |config|
-		config.cloud_name: 'dejrtx1pr'
-		config.api_key: '952536564313238'
-		config.api_secret: 'a0Mo73MHmNv21hBbkAeWJ5buWb4'
-		config.cdn_subdomain: true
+		config.fog_credentials = {
+		cloud_name: 'dejrtx1pr'
+		api_key: '952536564313238'
+		api_secret: 'a0Mo73MHmNv21hBbkAeWJ5buWb4'
+		cdn_subdomain: true}
 	end
 end
