a661de6e94d726f95019147efefb429d04302d55
akshedu <akshedu@gmail.com>
2015-11-01 16:18:51 +0530

    added cloudinary support

--- config/initializers/carrier_wave.rb
+++
@@ -1,9 +0,0 @@
-if Rails.env.production?
-	CarrierWave.configure do |config|
-		config.fog_credentials = {
-		cloud_name: 'dejrtx1pr',
-		api_key: '952536564313238',
-		api_secret: 'a0Mo73MHmNv21hBbkAeWJ5buWb4',
-		cdn_subdomain: true}
-	end
-end
