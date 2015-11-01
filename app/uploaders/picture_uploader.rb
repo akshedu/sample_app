201680276d90bca1cb4b8b778508552ee1e55daa
akshedu <akshedu@gmail.com>
2015-11-01 16:13:13 +0530

    added cloudinary support

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -6,7 +6,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include Cloudinary::CarrierWave
   
-  process resize_to_limit: [400, 400]
+  # process resize_to_limit: [400, 400]
 
   # Choose what kind of storage to use for this uploader:
   
